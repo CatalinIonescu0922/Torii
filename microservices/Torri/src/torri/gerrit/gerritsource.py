32a9933222449ea8a9ea3924ecf312fa88303b83
CatalinIonescu0922 <cata.ionescu2003@gmail.com>
2026-06-11 22:15:57 +0300

    test token

--- microservices/Torri/src/torri/gerrit/gerritsource.py
+++ microservices/Torri/src/torri/gerrit/gerritsource.py
@@ -4,7 +4,7 @@ from shared.logger_setup import get_logger
 class GerritSource(BaseSource):
     """
     Separation layer between the scheduler and the Gerrit connection.
-    So we dont share the connection right away
+    So we dont share the connection 
     """
     name = "gerrit"
 
